d8046c943cc23fc72cf65025fff7d916d09ae1e0
Yiliang Wan <yiliangw@hotmail.com>
2024-01-29 17:59:45 +0800

    update make and gitignore

--- apps/chaos/multipaxos/obj_wsn430/ifft.d
+++
@@ -1,8 +0,0 @@
-obj_wsn430/ifft.o: ../../../core/lib/ifft.c ../../../core/./lib/ifft.h \
- ../../../platform/wsn430/./contiki-conf.h \
- ../../../platform/wsn430/./platform-conf.h \
- ../../../cpu/msp430/./msp430def.h project-conf.h
-../../../core/lib/ifft.c ../../../core/./lib/ifft.h :
- ../../../platform/wsn430/./contiki-conf.h :
- ../../../platform/wsn430/./platform-conf.h :
- ../../../cpu/msp430/./msp430def.h project-conf.h :
--- apps/chaos/multipaxos/obj_wsn430/linkaddr.d
+++
@@ -1,8 +0,0 @@
-obj_wsn430/linkaddr.o: ../../../core/net/linkaddr.c \
- ../../../core/./net/linkaddr.h ../../../platform/wsn430/./contiki-conf.h \
- ../../../platform/wsn430/./platform-conf.h \
- ../../../cpu/msp430/./msp430def.h project-conf.h
-../../../core/net/linkaddr.c :
- ../../../core/./net/linkaddr.h ../../../platform/wsn430/./contiki-conf.h :
- ../../../platform/wsn430/./platform-conf.h :
- ../../../cpu/msp430/./msp430def.h project-conf.h :
